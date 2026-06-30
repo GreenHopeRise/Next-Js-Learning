d7206462efb3af8649d2f8f4f68ba9b86a9a73c8
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-06-30 23:17:29 +0600

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Home</h1>
+      <nav>
+        <Link href='/'>Home</Link>
+        <br />
+
+        <Link href="/about">About</Link>
+        <br />
+
+        <Link href="/contact">Contact</Link>
+        <br />
+
+        <Link href="/dashboard">Dashboard</Link>
+      </nav>
       
     </div>
   );
