6fcde66abdf6ab960afd9377d7871f19cd277353
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-07-04 22:05:15 +0600

    tt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from "next";
 import Footer from "./footer/page";
 import "./globals.css";
 import Navbar from "./navbar/page";
 
+
+export const metadata: Metadata={
+  title: "Khalid Next Course",
+  description: "Learning Next.js App Router",
+}
 export default function RootLayout({
   children,
 }: Readonly<{
