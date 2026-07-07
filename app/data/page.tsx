431485a467e42387a79d97f8d8046b27b64eb5af
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-07-07 09:35:33 +0600

    data fetching

---
+++ app/data/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default async function page() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const user = await res.json()
+  return (
+    <div>
+        {
+            user.map((u:any)=>(
+                <p key={u.id}>{u.name }$$</p>
+            ))
+        }
+    </div>
+  )
+}
